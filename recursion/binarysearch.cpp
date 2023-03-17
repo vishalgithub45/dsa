3b8e0471075ab33ae621ac1bd1ce9e53cca3aa48
Vishal <lokarevishal5767@gmail.com>
2023-03-18 01:40:42 +0530

    add number to array

--- recursion/binarysearch.cpp
+++ recursion/binarysearch.cpp
@@ -1,34 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool binarySearch(int *arr, int s, int e, int k){
-    //base case
+bool binarySearch(int *arr, int s, int e, int k)
+{
+    // base case
 
-    //element not found
+    // element not found
 
-    if(s>e)
+    if (s > e)
         return false;
-    
-    int mid = s + (e - s)/2;
 
-    // element found 
-    if(arr[mid] == k)
-    return true;
+    int mid = s + (e - s) / 2;
 
-    if(arr[mid]<k){
-        return binarySearch(arr, mid+1, e, k);
+    // element found
+    if (arr[mid] == k)
+        return true;
+
+    if (arr[mid] < k)
+    {
+        return binarySearch(arr, mid + 1, e, k);
     }
-    else{
-        return binarySearch(arr,s, mid-1,k);
+    else
+    {
+        return binarySearch(arr, s, mid - 1, k);
     }
 }
 
-int main(){
-    int arr[6] = {2,4,6,10,14,18};
+int main()
+{
+    int arr[6] = {2, 4, 6, 10, 14, 18, 12};
     int size = 6;
     int key = 15;
 
-    cout<<"present or not"<< binarySearch(arr, 0, 5, key) << endl;
+    cout << "present or not" << binarySearch(arr, 0, 5, key) << endl;
 
     return 0;
 }
