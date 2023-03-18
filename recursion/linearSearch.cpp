7e471b1c277aaf107d43dd26eeeb4c16dc5ea4e6
Vishal <lokarevishal5767@gmail.com>
2023-03-18 11:20:05 +0530

    changed array value

--- recursion/linearSearch.cpp
+++ recursion/linearSearch.cpp
@@ -1,40 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool linearSearch(int arr[],int size,int k){
+bool linearSearch(int arr[], int size, int k)
+{
 
-    // base case 
+  // base case
 
-    if(size == 0)
+  if (size == 0)
     return false;
 
-    if(arr[0] == k)
+  if (arr[0] == k)
   {
     return true;
   }
-  else{
-    bool remainingPart = linearSearch(arr+1, size-1,k);
+  else
+  {
+    bool remainingPart = linearSearch(arr + 1, size - 1, k);
     return remainingPart;
   }
-
-
 }
 
+int main()
+{
+  int arr[6] = {3, 5, 6, 1, 2, 56};
+  int size = 5;
+  int key = 6;
 
-int main() {
-    int arr[5] ={3,5,6,1,2};
-    int size = 5;
-    int key = 6;
+  bool ans = linearSearch(arr, size, key);
 
-    bool ans = linearSearch(arr,size,key);
-   
-    if(ans) {
-        cout<<"Present"<<endl;
+  if (ans)
+  {
+    cout << "Present" << endl;
+  }
+  else
+  {
+    cout << "absent" << endl;
+  }
 
-    }
-    else{
-        cout<<"absent"<<endl;
-    }
-
-    return 0;
+  return 0;
 }
